d6da2e44b68eabf713a8f3a55c145b817a9bf85b
Abhishek Kumar Rai <callmeabhishek2003@gmail.com>
2025-02-10 16:54:56 +0530

    second commit

--- Module 09/EvenOdd.java
+++ Module 09/EvenOdd.java
@@ -1,12 +1,19 @@
+                    //EVEN & ODD
 import java.util.Scanner;
 public class EvenOdd {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter a number: ");
+
+        System.out.println("enter a number : ");
         int n = sc.nextInt();
 
-        if(n%2 == 0) System.out.println("even");
-        
-    }
-    
+        if(n%2 == 0) {
+        System.out.println("even");
+        }
+        if (n%2 == 1) {
+            System.out.println("odd");
+        }
+    } 
 }
+
+
---
+++ Module 09/Relational.java
@@ -0,0 +1,42 @@
+/*Relational operators
+ * ==
+ * !=
+ * >=
+ * <=
+ */
+
+
+// public class Relational {
+//     public static void main(String[] args) {
+//         int a = 5;
+//         int b = 9;
+//         System.out.println(a == b);  //equal to
+//     }
+// }
+
+
+// public class Relational {
+//     public static void main(String[] args) {
+//         int a = 5;
+//         int b = 9;
+//         System.out.println(a != b); //not equal to
+//     }
+// }
+
+
+// public class Relational {
+//     public static void main(String[] args) {
+//         int a = 5;
+//         int b = 9;
+//         System.out.println(a >= b);  //greater then or equal to
+//     }
+// }
+
+
+public class Relational {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 9;
+        System.out.println(a <= b);  //smaller then or equal to
+    }
+}
