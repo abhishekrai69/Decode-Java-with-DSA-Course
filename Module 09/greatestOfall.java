bdc055716c06d49b0160d5f93ed0d1c580414727
Abhishek Kumar Rai <callmeabhishek2003@gmail.com>
2025-02-13 23:58:18 +0530

    six commit

---
+++ Module 09/greatestOfall.java
@@ -0,0 +1,8 @@
+//take 3 positive integer input and print the greatest of them...
+import java.util.Scanner;
+public class greatestOfall {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+    }
+}
