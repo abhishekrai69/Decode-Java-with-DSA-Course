2e8ea4b52931a003c114c55ced71e292c1e4b0b9
Abhishek Kumar Rai <callmeabhishek2003@gmail.com>
2025-03-03 23:53:43 +0530

    3 march 2025

---
+++ Module 09/if_else.java
@@ -0,0 +1,14 @@
+//Take positive integer input and tell if it is even or odd
+//write a program to find even or odd numbers
+import java.util.Scanner;
+public class if_else {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int money=9;
+        if (money >=0) {
+            System.out.println("you are rich");
+        }
+        
+    }
+}
